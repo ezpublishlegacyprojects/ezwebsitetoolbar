3e439a1994316ba08ed98c6dcfdeab3630f04905
ls <info@example.com>
2010-10-15 08:32:06 +0000

    - Tagged version/1.2.0
    git-svn-id: http://svn.projects.ez.no/ezwebsitetoolbar@38 036ce2ec-5c54-4936-9182-039a592c728d

---
+++ versions/1.2.0/packages/ezwt_extension/ezextension/ezwt/classes/ezwtservercallfunctions.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * eZ JS Core server call file for eZ Websitetoolbar
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ *
+ */
+
+/**
+ * Implements methods called remotely by sending XHR calls
+ *
+ */
+class ezwtServerCallFunctions
+{
+    /**
+     * Updating priority sorting for given node
+     *
+     * @param mixed $args
+     * @return array
+     */
+    public static function updatePriority( $args )
+    {
+        $http = eZHTTPTool::instance();
+
+        if ( !$http->hasPostVariable('ContentNodeID')
+                || !$http->hasPostVariable('PriorityID')
+                    || !$http->hasPostVariable('Priority') )
+        {
+            return array();
+        }
+
+        $contentNodeID = $http->postVariable('ContentNodeID');
+        $priorityArray = $http->postVariable('Priority');
+        $priorityIDArray = $http->postVariable('PriorityID');
+
+        if ( eZOperationHandler::operationIsAvailable( 'content_updatepriority' ) )
+        {
+            $operationResult = eZOperationHandler::execute( 'content', 'updatepriority',
+                                                             array( 'node_id' => $contentNodeID,
+                                                                    'priority' => $priorityArray,
+                                                                    'priority_id' => $priorityIDArray ), null, true );
+        }
+        else
+        {
+            eZContentOperationCollection::updatePriority( $contentNodeID, $priorityArray, $priorityIDArray );
+        }
+
+        if ( $http->hasPostVariable( 'ContentObjectID' ) )
+        {
+            $objectID = $http->postVariable( 'ContentObjectID' );
+            eZContentCacheManager::clearContentCache( $objectID );
+        }
+    }
+}
+
+?>
